83536b294cce65de447784d6ee98d89353341e7c
Fytyny <marcin123332@hotmail.com>
2016-12-15 11:10:00 +0100

    asf

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-	function change(){
-			
-			var s = document.getElementById("first");
-			var x = document.documentElement.clientWidth;
-			if (document.documentElement.scrollTop>130 && x>334){
-				s.style.display = "inline";
-				
-				
-			} else{
-				
-				s.style.display = "none";
-				
-			}
-			
+
+function change(){
+		var x = $(window);
+		var s = $("#first");
+		if (x.scrollTop()>130 && $(window).width()>334 ){
+			s.fadeIn("fast");
+		} else{
+			s.fadeOut("fast");
 		}
+	}
+$(document).ready(function(){ 
+	$("#podpis").hide();
+    var div = $("#txt");
+    var i = 0; 
+    var j = 0;
+    $("#pierwsze").mouseenter(function(){ 
+			hidesomeinfo($(".bla"));
+    	 	var div = $("#txt");
+       	  	showsomeinfo(div);
+        }); 
+    
+    $("#drugie").mouseenter(function(){ 
+			hidesomeinfo($(".bla"));
+    	    var div = $("#txt2");
+		    var p = $("#drugie").position();
+			div.css("left", p.left);
+       		showsomeinfo(div);
+    
+   }); 
+   $(".bla").click(function(){
+	   hidesomeinfo($(".bla"));
+   });
+   
+   $("#git").hover(function(){
+	   $("#podpis").slideDown();
+	   
+   },function(){
+	   $("#podpis").slideUp();
+   });
+ 
+});
+	 
+function showsomeinfo (div){
+			div.animate({height: '446px', opacity: '0.4'}, "fast"); 
+        	div.animate({width: '100%', opacity: '1',left: '0px'} ,500); 
+}
+function hidesomeinfo(div){
+			div.animate({height: '0px', opacity: '0',width: '200px'}, "fast"); 
+		
+}
